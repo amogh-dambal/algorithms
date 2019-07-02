6529cf6792b76e351d9ca5eec3a1d58be0bd285b
Amogh Dambal <amdam2013@gmail.com>
2019-07-01 20:19:01 -0500

    project euler solution 1

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "multiples_of_3_and_5.h"
+#include "stdio.h"
+
+int main()
+{
+    printf("%d\n", sum_of_multiples());
+}
---
+++ multiples_of_3_and_5.h
@@ -0,0 +1,14 @@
+
+const int sum_of_multiples()
+{
+    int sum = 0;
+    int i;
+    for (i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0) 
+        {
+            sum += i;
+        }
+    }
+    return sum;
+}
